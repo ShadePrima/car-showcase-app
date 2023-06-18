3ac0c0f39e434d087c1d5d9e24af24ead8c47a37
ShadePrima <aleksandrag1984@gmail.com>
2023-06-18 19:12:03 +0300

    manufacturer

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -29,9 +29,9 @@ const Hero = () => {
       <div className='hero__image-container'>
         <div className='hero__image'>
           <Image src='/hero.png' alt='car' fill className='object-contain' />
-
-          <div className='hero__image-overlay' />
         </div>
+
+        <div className='hero__image-overlay' />
       </div>
     </div>
   )
