cef46fbed921f85d1947000dd6b6686e8f610467
ShadePrima <aleksandrag1984@gmail.com>
2023-06-18 13:56:52 +0300

    footer

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,12 @@
+'use client'
+
 import React from 'react'
+import CustomButton from './CustomButton'
+import Image from 'next/image'
 
 const Hero = () => {
+  const handleScroll = () => {}
+
   return (
     <div className='hero'>
       <div className='flex-1 pt-36 padding-x'>
@@ -12,6 +18,20 @@ const Hero = () => {
           Srteamline your car rental experience with our effortless booking
           process.
         </p>
+
+        <CustomButton
+          title='Explore Cars'
+          containerStyles='bg-primary-blue text-white rounded-full mt-10'
+          handleClick={handleScroll}
+        />
+      </div>
+
+      <div className='hero__image-container'>
+        <div className='hero__image'>
+          <Image src='/hero.png' alt='car' fill className='object-contain' />
+
+          <div className='hero__image-overlay' />
+        </div>
       </div>
     </div>
   )
